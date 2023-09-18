c5737cb699c3ac14d1302a55f1ca6f26cc5e835c
luigitarallo <tarallo.gigi@gmail.com>
2023-09-18 16:52:30 +0200

    feat: add transition and overview

--- src/components/baseComponents/MediaCard.vue
+++ src/components/baseComponents/MediaCard.vue
@@ -6,69 +6,108 @@ export default {
 
 <template>
   <div class="col">
-    <div class="media-image">
-      <img
-        v-if="cardInfo.cardImage != 'https://image.tmdb.org/t/p/w342null'"
-        :src="cardInfo.cardImage"
-        alt=""
-      />
-      <img v-else src="img/not-found-icon.jpg" alt="" />
-    </div>
-    <div class="d-none">
-      {{ cardInfo.name }}
-      <div class="flag-image">
-        <img :src="cardInfo.language" alt="" />
-      </div>
-      {{ cardInfo.original_title }} -
-      <div class="stars">
-        <font-awesome-icon
-          icon="fa-solid fa-star"
-          v-for="n in cardInfo.vote"
-          :key="cardInfo.id"
-          class="star-full"
-        />
+    <div class="media-card">
+      <div class="media-card-content">
+        <div class="media-card-img">
+          <img
+            v-if="cardInfo.cardImage != 'https://image.tmdb.org/t/p/w342null'"
+            :src="cardInfo.cardImage"
+            :alt="cardInfo.name"
+          />
+          <img
+            v-else
+            src="public\img\image-not-available.png"
+            alt="generale image"
+          />
+        </div>
 
-        <font-awesome-icon
-          icon="fa-solid fa-star"
-          v-for="n in 5 - cardInfo.vote"
-          :key="cardInfo.id"
-          class="star-empty"
-        />
+        <div class="media-card-info">
+          <h2>Titolo: {{ cardInfo.name }}</h2>
+          <h3>Titolo originale: {{ cardInfo.original_title }}</h3>
+          <span
+            >Lingua:<img
+              :src="cardInfo.language"
+              alt="language"
+              class="flag-image"
+            />
+          </span>
+          <p>{{ cardInfo.overview }}</p>
+
+          <div class="stars">
+            <font-awesome-icon
+              icon="fa-solid fa-star"
+              v-for="n in cardInfo.vote"
+              :key="cardInfo.id"
+              class="star-full"
+            />
+
+            <font-awesome-icon
+              icon="fa-solid fa-star"
+              v-for="n in 5 - cardInfo.vote"
+              :key="cardInfo.id"
+              class="star-empty"
+            />
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-col {
+@use "../../assets/scss/general.scss";
+
+.media-card {
+  background-color: transparent;
+  perspective: 1000px;
   width: 100%;
-  height: 100%;
+  height: 460px;
 }
-.media-image {
-  display: flex;
-  align-items: center;
+
+.media-card-content {
+  position: relative;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+.media-card:hover .media-card-content {
+  transform: rotateY(180deg);
+}
+
+.media-card-img,
+.media-card-info {
+  position: absolute;
   width: 100%;
-  height: 100%;
+  height: 460px;
+  backface-visibility: hidden;
+}
+
+.media-card-img {
+  object-fit: cover;
+
   img {
+    max-width: 100%;
+    height: 460px;
+    object-fit: cover;
     border: 1px solid red;
     border-radius: 5px;
-    object-fit: cover;
-  }
-  .media-not-found {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
   }
 }
-.flag-image {
-  height: 30px;
-  width: 30px;
+
+.media-card-info {
+  background-color: black;
+  color: white;
+  transform: rotateY(180deg);
+  border-radius: 5px;
+  border: 1px solid red;
+  .flag-image {
+    width: 30px;
+  }
 }
-.star-full {
+
+.media-card-info .star-full {
   color: yellow;
 }
 .star-empty {
-  color: black;
+  color: white;
 }
 </style>
