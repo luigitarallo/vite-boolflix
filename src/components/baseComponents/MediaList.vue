c46f171de7f8ecb529e41980e0b8637de2dae95e
luigitarallo <tarallo.gigi@gmail.com>
2023-09-18 16:52:10 +0200

    feat: add section

--- src/components/baseComponents/MediaList.vue
+++ src/components/baseComponents/MediaList.vue
@@ -14,25 +14,33 @@ export default {
 
 <template>
   <div class="container">
-    <h2>Film</h2>
-    <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 gy-3">
-      <MediaCard
-        v-for="movie in store.movies"
-        :key="movie.id"
-        :cardInfo="movie"
-      />
-    </div>
-  </div>
-  <div class="container">
-    <h2>Serie TV</h2>
-    <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 gy-3">
-      <MediaCard
-        v-for="serie in store.series"
-        :key="serie.id"
-        :cardInfo="serie"
-      />
-    </div>
+    <section>
+      <h2>Film</h2>
+      <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 gy-3">
+        <MediaCard
+          v-for="movie in store.movies"
+          :key="movie.id"
+          :cardInfo="movie"
+        />
+      </div>
+    </section>
+    <section>
+      <h2>Serie TV</h2>
+      <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 gy-3">
+        <MediaCard
+          v-for="serie in store.series"
+          :key="serie.id"
+          :cardInfo="serie"
+        />
+      </div>
+    </section>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../../assets/scss/general.scss";
+h2 {
+  color: white;
+  padding: 1rem 0;
+}
+</style>
